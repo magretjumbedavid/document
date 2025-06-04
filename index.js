619c9f15c6fc53bf73b3d2e8d129e54aae6d3f54
magretjumbedavid <magretjumbedavid@gmail.com>
2025-06-04 14:49:33 +0300

    all

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+ document.body.style.backgroundColor = "gray";
+ title.style.color = "green";
+ 
+
+ 
+ 
+
+//  adding fruit
+const newFruit = document.createElement('pineapple');
+fruitList.appendChild(newFruit);
+newFruit.textContent = 'pineapple';
+newFruit.setAttribute('class','new-Fruit');
+
+//adding vegetable
+
+const newVegetable = document.createElement('Mustard');
+vegList.appendChild(newVegetable);
+newVegetable.textContent = 'Mustard';
+newVegetable.setAttribute('class','new-Vegetable');
