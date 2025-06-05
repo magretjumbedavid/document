f85917521c676b81223cd5068faeff2bdd1f4b9a
magretjumbedavid <magretjumbedavid@gmail.com>
2025-06-05 07:05:50 +0300

    initial

--- index.js
+++ index.js
@@ -8,14 +8,22 @@
  
 
 //  adding fruit
-const newFruit = document.createElement('pineapple');
+const newFruit = document.createElement('li');
 fruitList.appendChild(newFruit);
 newFruit.textContent = 'pineapple';
 newFruit.setAttribute('class','new-Fruit');
 
 //adding vegetable
 
-const newVegetable = document.createElement('Mustard');
+const newVegetable = document.createElement('li');
 vegList.appendChild(newVegetable);
 newVegetable.textContent = 'Mustard';
 newVegetable.setAttribute('class','new-Vegetable');
+
+
+const upper=document.getElementById("upper");
+upper.style.textTransform="uppercase";
+
+
+const ase=document.getElementById("ase");
+ase.style.textTransform="upperCase";
